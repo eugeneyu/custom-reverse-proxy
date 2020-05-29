7bdef8ef721c8db50b7c1c3894a7063a5089c36c
Eugene Yu <youzhi.yu@gmail.com>
2020-05-29 16:28:20 +0800

    Fix variable replacement issue

--- filebeat/deploy_filebeat.sh
+++ filebeat/deploy_filebeat.sh
@@ -11,6 +11,7 @@ sudo filebeat modules enable nginx
 
 wget https://raw.githubusercontent.com/eugeneyu/custom-reverse-proxy/master/filebeat/filebeat.yml --output-document=./filebeat.yml
 
-envsubst '${ES_USER} ${ES_PASSWORD}' < ./filebeat.yml > /etc/filebeat/filebeat.yml
+envsubst '${ES_USER} ${ES_PASSWORD}' < ./filebeat.yml > ./filebeat_update.yml
+sudo cp ./filebeat_update.yml /etc/filebeat/filebeat.yml
 
 sudo service filebeat start
