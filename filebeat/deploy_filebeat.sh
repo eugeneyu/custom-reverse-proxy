c1f8324b3dc9f3fe5be77aba28d8ac5f7f24e0af
Eugene Yu <youzhi.yu@gmail.com>
2020-05-29 14:49:03 +0800

    update deploy_filebeat.sh

--- filebeat/deploy_filebeat.sh
+++ filebeat/deploy_filebeat.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
 # Before execution, set up environment variables
-# export ES_USER=eugeneyu
-# export ES_PASSWORD=abc
+export ES_USER=eugeneyu
+export ES_PASSWORD=abc
 
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.7.0-amd64.deb
 sudo dpkg -i filebeat-7.7.0-amd64.deb
 
 sudo filebeat modules enable nginx
+
+wget https://raw.githubusercontent.com/eugeneyu/custom-reverse-proxy/master/filebeat/filebeat.yml --output-document=./filebeat.yml
 
 envsubst '${ES_USER} ${ES_PASSWORD}' < ./filebeat.yml > /etc/filebeat/filebeat.yml
 
